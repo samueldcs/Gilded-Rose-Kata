ddfb207fda171034c61147836b58a755ccc78d94
Samuel Simão <mucapapipa.br@gmail.com>
2020-10-17 17:06:25 -0300

    simplify factory by replacing enum with a simple map

--- src/main/java/org/gildedrose/qualitycontrol/QualityControlFactory.java
+++ src/main/java/org/gildedrose/qualitycontrol/QualityControlFactory.java
@@ -2,38 +2,37 @@ package org.gildedrose.qualitycontrol;
 
 import org.gildedrose.Item;
 
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
 public class QualityControlFactory {
+	
+	private static final String EMPTY_STRING = "";
 	
 	public static final String BACKSTAGE_PASS_ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert";
 	public static final String SULFURAS_ITEM_NAME = "Sulfuras, Hand of Ragnaros";
 	public static final String AGED_BRIE_ITEM_NAME = "Aged Brie";
 	public static final String CONJURED_ITEM_NAME = "Conjured Mana Cake";
 	
-	private enum ItemQualityControl {
-		AGED_BRIE(AGED_BRIE_ITEM_NAME, new AgedBrieQualityControl()),
-		BACKSTAGE_PASS(BACKSTAGE_PASS_ITEM_NAME, new BackstagePassQualityControl()),
-		CONJURED(CONJURED_ITEM_NAME, new ConjuredQualityControl()),
-		SULFURAS(SULFURAS_ITEM_NAME, new SulfurasQualityControl());
-		
-		private final String itemName;
-		private final QualityControl qualityControl;
-		
-		ItemQualityControl(String itemName, QualityControl qualityControl) {
-			this.itemName = itemName;
-			this.qualityControl = qualityControl;
-		}
-		
-		public static QualityControl qualityControlFor(Item item) {
-			for (ItemQualityControl itemQualityControl : ItemQualityControl.values())
-				if (itemQualityControl.itemName.equals(item.getName())) {
-					return itemQualityControl.qualityControl;
-				}
-			return new DefaultQualityControl();
-		}
-	}
+	private final Map<String, QualityControl> qualityControls =
+			Map.of(AGED_BRIE_ITEM_NAME, new AgedBrieQualityControl(),
+					BACKSTAGE_PASS_ITEM_NAME, new BackstagePassQualityControl(),
+					CONJURED_ITEM_NAME, new ConjuredQualityControl(),
+					SULFURAS_ITEM_NAME, new SulfurasQualityControl());
 	
 	public QualityControl qualityControlFor(Item item) {
-		return ItemQualityControl.qualityControlFor(item);
+		return qualityControls
+				.getOrDefault(
+						getItemName(item),
+						new DefaultQualityControl()
+				);
+	}
+	
+	private String getItemName(final Item item) {
+		return ofNullable(item.name)
+				.orElse(EMPTY_STRING);
 	}
 	
 }
